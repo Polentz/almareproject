06406b29f43f063f04b6be5c5a39b6e07cb2b616
Giulia Polenta <giuliapolenta@gmail.com>
2024-03-14 13:47:00 +0100

    Add control over video attributes

--- site/snippets/blocks/video.php
+++ site/snippets/blocks/video.php
@@ -10,7 +10,12 @@ use Kirby\Cms\Html;
 </figure>
 <?php elseif ($video = $block->video()->toFile()) : ?>
     <figure class="video-block">
-        <video src="<?= $video->url() ?>" controls autoplay muted loop controlslist="noplaybackrate nodownload" disablePictureInPicture type="video"></video>
+        <?php $poster = $block->poster()->toFile() ?>
+        <video src="<?= $video->url() ?>" controls loop controlslist="noplaybackrate nodownload" disablePictureInPicture
+        <?php if ($block->toggle()->toBool() === true) : ?>
+            autoplay muted
+        <?php endif ?>
+        <?php if ($poster) : ?> poster="<?= $poster->url() ?>"<?php endif ?> type="video"></video>
         <?php if ($block->caption()->isNotEmpty()): ?>
             <figcaption><?= $block->caption() ?></figcaption>
         <?php endif ?>
